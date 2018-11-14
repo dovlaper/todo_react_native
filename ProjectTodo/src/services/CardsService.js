75feca7e1fcfec06c62cde3de70c53ce668f2b05
Vladimir Antonic <vladimir.a@vivifyideas.com>
2018-11-14 10:32:08 +0100

    #LVAI-16 Bug fix

--- ProjectTodo/src/services/CardsService.js
+++ ProjectTodo/src/services/CardsService.js
@@ -11,7 +11,7 @@ class CardsService extends BaseService {
   };
 
   addNewCard = data => {
-    this.apiClient().post(ENDPOINTS.CREATE_CARD, data);
+    return this.apiClient().post(ENDPOINTS.CREATE_CARD, data);
   };
 }
 
